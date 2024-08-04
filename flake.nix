ff97b11ecf8104f64c10784acabf77e7701b57de
Frederik Richter <frederik.richter@mailbox.org>
2024-08-04 12:29:40 +0200

    pin nix pkgs commit hash

--- flake.nix
+++ flake.nix
@@ -1,37 +1,37 @@
 {
   description = "Git Annex Docker image based on NixOS";
 
-inputs = {
+  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     nixpkgs.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.inputs.nixpkgs.url = "github:NixOS/nixpkgs/commit/05405724efa137a0b899cce5ab4dde463b4fd30b";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.dockerTools.buildLayeredImage {
-          name = "git-annex";
-          tag = "latest";
-          contents = with pkgs; [
-            git
-            git-annex
-            bash
-            coreutils
-            gnupg
-            openssh
-          ];
-          config = {
-            Cmd = [ "${pkgs.git-annex}/bin/git-annex" ];
-            WorkingDir = "/data";
-            Volumes = {
-              "/data" = {};
-            };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.dockerTools.buildLayeredImage {
+        name = "git-annex";
+        tag = "latest";
+        contents = with pkgs; [
+          git
+          git-annex
+          bash
+          coreutils
+          gnupg
+          openssh
+        ];
+        config = {
+          Cmd = [ "${pkgs.git-annex}/bin/git-annex" ];
+          WorkingDir = "/data";
+          Volumes = {
+            "/data" = {};
           };
         };
-      }
-    );
+      };
+    };
 }
